9e8639fca4318555fe89d8cfd323c65fa4def44a
KRISH DHIMAN <krrishdhiman841@gmail.com>
2025-03-11 23:07:00 +0530

    Daily code push

--- backend/services/cronJob.js
+++ backend/services/cronJob.js
@@ -8,8 +8,18 @@ cron.schedule('0 0 * * *', async () => { // Run At Every day at midnight
     console.log('Deleted unverified users...')
 })
 
-cron.schedule('0 23 * * *', () => { // Run every day at 11 PM
+cron.schedule('* * * * *', () => { // Run every day at 11 PM
     exec('git add . && git commit -m "Daily code push" && git push', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing git push: ${error}`);
+            return;
+        }
+        console.log(`Git push output: ${stdout}`);
+        if (stderr) {
+            console.error(`Git push error output: ${stderr}`);
+        }
+    }),
+    exec('cd frontend && git add . && git commit -m "Daily code push" && git push', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing git push: ${error}`);
             return;
